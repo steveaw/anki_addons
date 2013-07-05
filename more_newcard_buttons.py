2cbb09028b7ac33fca074530863ab59476a74ddc
steveaw <swaring@ozemail.com.au>
2013-07-05 16:54:41 +1000

    work around for css problem

--- more_newcard_buttons.py
+++ more_newcard_buttons.py
@@ -50,12 +50,12 @@ This can be edited to suit, but there can not be more than 5 buttons.
     ReschedMax ... same as the higher number in the Browser's "Edit/Rescedule" command
 """
 
-extra_buttons = [{"Description":"3-4d","Label":"3-4","ShortCut":"5","ReschedMin":3,"ReschedMax":5} ,
-                {"Description":"5-7d", "Label":"5-7","ShortCut":"6","ReschedMin":5,"ReschedMax":7} , 
-                {"Description":"8-15d","Label":"8-15","ShortCut":"7","ReschedMin":8,"ReschedMax":15} ,
-                {"Description":"1N","Label":"1N","ShortCut":"","ReschedMin":1,"ReschedMax":1}]
+extra_buttons = [{"Description": "3-4d", "Label": "3-4", "ShortCut": "5", "ReschedMin": 3, "ReschedMax": 5},
+                 {"Description": "5-7d", "Label": "5-7", "ShortCut": "6", "ReschedMin": 5, "ReschedMax": 7},
+                 {"Description": "8-15d", "Label": "8-15", "ShortCut": "7", "ReschedMin": 8, "ReschedMax": 15},
+                 {"Description": "1N", "Label": "1N", "ShortCut": "", "ReschedMin": 1, "ReschedMax": 1}]
 #Must be four or less
-assert len (extra_buttons) <= 4
+assert len(extra_buttons) <= 4
 
 from aqt.reviewer import Reviewer
 from aqt.utils import showInfo
@@ -63,12 +63,13 @@ from anki.hooks import wrap
 
 #Anki uses a single digit to track which button has been clicked.
 #We will use 6 and above to track the extra buttons.
-INTERCEPT_EASE_BASE =6
+INTERCEPT_EASE_BASE = 6
 
 #todo: brittle. Replaces existing function
 def _answerButtons(self):
     times = []
     default = self._defaultEase()
+
     def but(i, label):
         if i == default:
             extra = "id=defease"
@@ -78,10 +79,11 @@ def _answerButtons(self):
         return '''
 <td align=center>%s<button %s title="%s" onclick='py.link("ease%d");'>\
 %s</button></td>''' % (due, extra, _("Shortcut key: %s") % i, i, label)
+
     buf = "<center><table cellpading=0 cellspacing=0><tr>"
     for ease, label in self._answerButtonList():
         buf += but(ease, label)
-    #swAdded start ====>
+        #swAdded start ====>
     #Only for cards in the new queue
     if self.card.type == 0:
         #Check that the number of answer buttons is as expected.
@@ -90,8 +92,8 @@ def _answerButtons(self):
         for i, buttonItem in enumerate(extra_buttons):
             buf += '''
 <td align=center><span class=nobold>%s</span><br><button title="Short key: %s" onclick='py.link("ease%d");'>\
-%s</button></td>''' % (buttonItem["Description"] , buttonItem["ShortCut"], i + INTERCEPT_EASE_BASE , buttonItem["Label"])
-    #swAdded end
+%s</button></td>''' % (buttonItem["Description"], buttonItem["ShortCut"], i + INTERCEPT_EASE_BASE, buttonItem["Label"])
+        #swAdded end
     buf += "</tr></table>"
     script = """
 <script>$(function () { $("#defease").focus(); });</script>"""
@@ -109,13 +111,13 @@ def answer_card_intercepting(self, actual_ease, _old):
         ease = 3
         #We will need this to reschedule it.
         prev_card_id = self.card.id
-    #
-    ret = _old(self,ease)
-    
+        #
+    ret = _old(self, ease)
+
     if is_extra_button:
-        buttonItem = extra_buttons[actual_ease-INTERCEPT_EASE_BASE]
+        buttonItem = extra_buttons[actual_ease - INTERCEPT_EASE_BASE]
         #Do the reschedule.
-        self.mw.col.sched.reschedCards([prev_card_id],buttonItem["ReschedMin"],buttonItem["ReschedMax"])
+        self.mw.col.sched.reschedCards([prev_card_id], buttonItem["ReschedMin"], buttonItem["ReschedMax"])
     return ret
 
 #Handle the shortcut. Used changekeys.py addon as a guide     
@@ -127,6 +129,7 @@ def keyHandler(self, evt, _old):
                 #early exit ok in python?
                 return self._answerCard(i + INTERCEPT_EASE_BASE)
     return _old(self, evt)
+
 
 Reviewer._keyHandler = wrap(Reviewer._keyHandler, keyHandler, "around")
 Reviewer._answerButtons = _answerButtons
